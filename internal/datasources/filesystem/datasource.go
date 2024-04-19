260cf69e0bb3948b4dd0cb6b85d234733392b906
Jun Siang Cheah <git@jscheah.me>
2024-04-19 16:38:22 +0100

    last go commit

--- internal/datasources/filesystem/datasource.go
+++ internal/datasources/filesystem/datasource.go
@@ -4,9 +4,8 @@ type Datasource struct {
 	// Path is the path to a directory containing files
 	Path string
 	// GlobPatterns is a list of glob patterns to match files if specified. If not specified, all files are matched.
+	//
 	GlobPatterns []string
-	// IgnorePatterns is a list of glob patterns to ignore files if specified. If not specified, no files are ignored.
-	IgnorePatterns []string
 	// RespectGitIgnore specifies whether to respect .gitignore files. Defaults to true.
 	RespectGitIgnore bool
 }
