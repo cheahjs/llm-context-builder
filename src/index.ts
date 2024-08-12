d037c08b67870d301d22efe9d0ac455491f62608
Jun Siang Cheah <git@jscheah.me>
2024-08-12 18:15:05 +0100

    refactor: Remove unused commands for git and web inputs

--- src/index.ts
+++ src/index.ts
@@ -11,30 +11,12 @@ program
   .option('--tokenizer <tokenizer>', 'Specify the tokenizer to use for counting tokens');
 
 program
-  .command('git <input>')
-  .description('Bundle a git repository')
-  .option('--include <patterns...>', 'Include files matching these glob patterns')
-  .option('--exclude <patterns...>', 'Exclude files matching these glob patterns')
-  .action((input, options) => {
-    // TODO: Implement logic to handle the git input and options
-  });
-
-program
   .command('file <input>')
   .description('Bundle a file or directory')
   .option('--include <patterns...>', 'Include files matching these glob patterns')
   .option('--exclude <patterns...>', 'Exclude files matching these glob patterns')
   .action((input, options) => {
     // TODO: Implement logic to handle the file input and options
-  });
-
-program
-  .command('web <input>')
-  .description('Bundle a website')
-  .option('--include <patterns...>', 'Include files matching these glob patterns')
-  .option('--exclude <patterns...>', 'Exclude files matching these glob patterns')
-  .action((input, options) => {
-    // TODO: Implement logic to handle the web input and options
   });
 
 program.parse(process.argv);
